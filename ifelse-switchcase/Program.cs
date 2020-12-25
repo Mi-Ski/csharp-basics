85a2fcf813161e47b5325a77e606bc9c2a785f6e
Mic-PC <skibamichal@interia.pl>
2020-12-25 03:29:13 +0100

    swithch/case

---
+++ ifelse-switchcase/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ifelse_switchcase
+{
+    public enum Seasons
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // bool isGoldCustomer = true;
+            
+            // // if (isGoldCustomer)
+            // // {
+            // //     price = 19.95f;
+            // // }
+            // // else
+            // // {
+            // //     price = 29.95f;
+            // // }
+            // float price = (isGoldCustomer) ? 12.21f : 53.31f;
+            // System.Console.WriteLine(price);
+            var season = Seasons.Autumn;
+            switch (season)
+            {
+                case Seasons.Autumn:
+                case Seasons.Winter:
+                    System.Console.WriteLine("wake me up when the weather's better");
+                    break;
+                
+                case Seasons.Spring:
+                    System.Console.WriteLine("s p r i n g");
+                    break;
+                
+                case Seasons.Summer:
+                    System.Console.WriteLine("getting a bit hot innit?");
+                    break;
+                
+                default:
+                    System.Console.WriteLine("There are only 4 seasons, insert a correct one, goofball!");
+                    break;
+            };
+        }
+    }
+}
---
+++ iteration-statements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace iteration_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for foreach while do-while
+            System.Console.WriteLine("for loop");
+            for (var i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine(i);
+                if(i >= 7) break;
+            }
+
+            System.Console.WriteLine("foreach loop");
+            var numbers = new int[5]{1,2,3,5,4};
+
+            foreach (var i in numbers)
+            {
+                System.Console.WriteLine(i);
+            }
+
+            // System.Console.WriteLine("while loop");
+            // while (true)
+            // {
+            //     System.Console.WriteLine(2);
+            // }
+
+            System.Console.WriteLine("do-while loop");
+            var a = 10;
+            do
+            {
+                ++a;
+                System.Console.WriteLine(a);
+            } while (a < 10);
+        }
+    }
+}
