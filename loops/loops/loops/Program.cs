ab3775903843645205565b4ca5ed5781562d3eeb
Mic-PC <skibamichal@interia.pl>
2020-12-28 22:08:31 +0100

    loops

---
+++ loops/loops/loops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   while (true)
+               {
+                   Console.WriteLine("Type your name: ");
+                   var input = Console.ReadLine();
+
+                   if (!String.IsNullOrWhiteSpace(input))
+                   {
+                       Console.WriteLine("@Echo: " + input);
+                       // program continues on the beginning of the loop
+                       continue;
+                   }
+                   break;
+               }*/
+
+            var random = new Random();
+            byte passwordLength = 12;
+            char[] buffer = new char[passwordLength];
+
+            for (var i = 0; i < passwordLength; i++)
+            {
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            }
+
+            var password = new string(buffer);
+            Console.WriteLine(password);
+
+        }
+    }
+}
