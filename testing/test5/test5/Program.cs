b1bd344690f3e639a2f7ee2b85bc48ed2a05f674
Mic-PC <skibamichal@interia.pl>
2021-01-09 11:52:26 +0100

    final assignment

---
+++ testing/test5/test5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace test5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(f(4));
+        }
+        static int f(int x)
+
+        {
+
+            if (x > 1) return f(x - 1) + 2 * f(x
+            - 2);
+
+            else return x;
+
+        }
+    }
+}
